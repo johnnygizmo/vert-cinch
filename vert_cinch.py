de2dd59898135d384531d40de391fd44446957eb
Johnny Matthews <johnny.matthews@gmail.com>
2018-03-28 11:26:29 -0500

    Add more features

--- vert_cinch.py
+++ vert_cinch.py
@@ -29,35 +29,104 @@ bl_info = {
 
 import bpy
 import bmesh
+import mathutils
 
-def main(self,context,distance,pairs):
+def main(self,context):
     me = bpy.context.object.data
-    bm = bmesh.from_edit_mesh(me)   
+    bm = bmesh.from_edit_mesh(me)       
 
     for s in bm.select_history:
+        print(type(s).__name__)
+
         if(type(s).__name__ != "BMVert"):
-            self.report({'ERROR_INVALID_INPUT'},"Not All Selections Were Verts")
+            self.report({'ERROR_INVALID_INPUT'},"Make a vertex selection only")
             return
 
+    if len(bm.select_history) == 0:
+        self.report({'ERROR_INVALID_INPUT'},"Make individual vertex selections, circle and box select do not work")
+        return
 
-    last = len(bm.select_history)-1
-    if pairs == False:
-        for idx,val in enumerate(bm.select_history):
-            if idx != last:
-                val.co = val.co.lerp(bm.select_history[last].co,distance)
-    else:
-        for idx,val in enumerate(bm.select_history):
-            if (idx % 2) == 0 and idx+1 <= last:
-                val.co = val.co.lerp(bm.select_history[idx+1].co,distance)
+    sct = 0
+    for v in bm.verts:
+        if v.select:
+            sct = sct + 1
     
+    if sct != len(bm.select_history):
+        self.report({'ERROR_INVALID_INPUT'},"Make individual vertex selections")
+        return
+
+
+    verts = len(bm.select_history)
+    last  = verts-1
+
+    if self.grouping == 'PAIRS':
+        if(verts % 2) != 0:
+            self.report({'ERROR_INVALID_INPUT'},"Odd number of verts selected. Last will be ignored")
+        
+        center = bm.select_history[0]
+        for idx,val in enumerate(bm.select_history):
+
+            if self.target == 'LAST': 
+                if (idx % 2) == 0 and idx+1 <= last:          
+                    val.co = val.co.lerp(bm.select_history[idx+1].co,self.distance)
+            if self.target == 'FIRST': 
+                if (idx % 2) == 1 and idx <= last:          
+                    val.co = val.co.lerp(bm.select_history[idx-1].co,self.distance)   
+            if self.target == 'MIDDLE': 
+                if (idx % 2) == 0 and idx+1 <= last:   
+                    center = val.co.lerp(bm.select_history[idx+1].co,0.5)
+                    val.co = val.co.lerp(center,self.distance)   
+                if (idx % 2) == 1 and idx <= last:   
+                    val.co = val.co.lerp(center,self.distance)  
+
+    elif self.grouping == 'ROWS':
+        half = verts // 2
+        
+        if self.target == 'LAST': 
+            for idx,val in enumerate(bm.select_history):
+                if idx < half:
+                    val.co = val.co.lerp(bm.select_history[idx+half].co,self.distance)
+        if self.target == 'FIRST': 
+            for idx,val in enumerate(bm.select_history):
+                if idx >= half:
+                    val.co = val.co.lerp(bm.select_history[idx-half].co,self.distance)  
+
+        if self.target == 'MIDDLE': 
+            for idx,val in enumerate(bm.select_history):
+                if idx < half:
+                    center = val.co.lerp(bm.select_history[idx+half].co,0.5)
+                    bm.select_history[idx].co      = bm.select_history[idx].co.lerp(center,self.distance)
+                    bm.select_history[idx+half].co = bm.select_history[idx+half].co.lerp(center,self.distance)                
+            
     
+    elif self.grouping == 'CLUSTER':
+        if self.target == 'FIRST': 
+            for idx,val in enumerate(bm.select_history):
+                if idx != 0:
+                    val.co = val.co.lerp(bm.select_history[0].co,self.distance)
+        elif self.target == 'LAST':        
+            for idx,val in enumerate(bm.select_history):
+                if idx != last:
+                    val.co = val.co.lerp(bm.select_history[last].co,self.distance)
+        elif self.target == "MIDDLE":
+            center = mathutils.Vector((0.0, 0.0, 0.0))
+            for idx,val in enumerate(bm.select_history):
+                center = center + val.co
+            center.x = center.x / verts
+            center.y = center.y / verts
+            center.z = center.z / verts
+            for idx,val in enumerate(bm.select_history):
+                val.co = val.co.lerp(center,self.distance)
+
+
                
     bmesh.update_edit_mesh(me) 
 
 
 from bpy.props import (
         BoolProperty,
-        FloatProperty
+        FloatProperty,
+        EnumProperty
         )
 
 
@@ -69,13 +138,28 @@ class VertCinch(bpy.types.Operator):
     
 
 ### TODO - Add a menu of Pairs, To Last, To First, Strips
-
-    pairs    = BoolProperty(
-        name = "Cinch Pairs",
-        description = "Cinch every other selected vert",
-        default = True  
-    )
     
+    grouping = EnumProperty(
+                name="Grouping",
+                description="Vertex Grouping",
+                default="PAIRS",
+                items=(
+                    ('PAIRS','Pairs','Cinch every other vertex selected'),
+                    ('ROWS','Rows','Cinch first half of selecte to 2nd half'),
+                    ('CLUSTER','Cluster','Cinch all selected to one vertex')
+                )
+    )
+    target = EnumProperty(
+                name="Target",
+                description="Cinch Target",
+                default="FIRST",
+                items=(
+                    ('FIRST','First','Cinch to First Vertex or Row'),
+                    ('LAST','Last','Cinch to Last Vertex or Row'),
+                    ('MIDDLE','Middle','Cinch to Midpoint')
+                )
+    )
+
     distance = FloatProperty(
             name="Distance",
             description="Cinching Distance",
@@ -88,7 +172,7 @@ class VertCinch(bpy.types.Operator):
         return context.active_object is not None
 
     def execute(self, context):
-        main(self,context,self.distance,self.pairs)
+        main(self,context)
         return {'FINISHED'}
 
 
